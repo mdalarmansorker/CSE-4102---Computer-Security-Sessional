edb4841584f49415b4cfbc8ffcce6d79cfe4c3ae
Arman <mdalarmansorker@gmail.com>
2023-03-18 22:20:16 +0600

    Shift cipher added

---
+++ shift_cipher.py
@@ -0,0 +1,30 @@
+key = 1
+def encriptions():
+    text = input("Text = ")
+    entext = ""
+    for letter in text:
+        if(letter.islower()):
+            entext += chr((ord(letter)+key-97)%26+97)
+        elif(letter.isupper()):
+            entext += chr((ord(letter)+key-65)%26+65)
+        else:
+            entext += letter
+       
+    text = entext
+    print("Encripted text: " + text)
+
+def decriptions():
+    text = input("Text = ")
+    detext = ""
+    for letter in text:
+        if(letter.islower()):
+            detext += chr((ord(letter)-key-97)%26+97)
+        elif(letter.isupper()):
+            detext += chr((ord(letter)-key-65)%26+65)
+        else:
+            detext += letter
+    text = detext
+    print("Decripted Text: " + text)
+
+encriptions()
+decriptions()
