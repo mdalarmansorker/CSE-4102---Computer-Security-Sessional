cf97f9fe6515f8cf2378be5b8d6fe31c7dd40c04
Arman <mdalarmansorker@gmail.com>
2023-03-18 22:44:36 +0600

    mono alphabetic cipher code fixed

--- mono_alphabetic_cipher.py
+++ mono_alphabetic_cipher.py
@@ -1,21 +1,15 @@
 pt = input("Enter the text: ")
-ept = []
 key = input("Enter the 26 length key: ")
-
+ept = []
 endict = {}
-dedict = {}
 
 for i in range(26):
     if(key[i].isupper()):
         endict[chr(65+i)] = key[i]
-        dedict[key[i]] = chr(65+i)
         endict[chr(97+i)] = key[i].lower()
-        dedict[key[i].lower()] = chr(97+i)
     else:
         endict[chr(97+i)] = key[i]
-        dedict[key[i]] = chr(97+i)
         endict[chr(65+i)] = key[i].upper()
-        dedict[key[i].upper()] = chr(65+i)
 
 for i in pt:
     if(i.isupper() or i.islower()):
@@ -27,14 +21,23 @@ ept = ''.join(ept)
 print(ept)
 
 enpt = input("Enter the encrpted string: ")
-# encripted_key = input("Enter encripted key: ")
-# print(dedict['z'])
+encripted_key = input("Enter encripted key: ")
 dpt = []
+dedict = {}
+for i in range(26):
+    if(key[i].isupper()):
+        dedict[key[i]] = chr(65+i)
+        dedict[key[i].lower()] = chr(97+i)
+    else:
+        dedict[key[i]] = chr(97+i)
+        dedict[key[i].upper()] = chr(65+i)
+
 for i in enpt:
     if(i.isupper() or i.islower()):
         dpt.append(dedict[i])
     else:
         dpt.append(i)
+        
 dpt = ''.join(dpt)
 print(dpt)
     
