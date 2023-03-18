5851a110046107d2b4c3308fbba5cf1fe4450226
Arman <mdalarmansorker@gmail.com>
2023-03-18 22:22:13 +0600

    Caesar cipher code changed by modulus

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -3,22 +3,10 @@ def encriptions():
     text = input("Text = ")
     entext = ""
     for letter in text:
-        if(letter == " "):
-            entext += letter
-            continue
-        elif(letter.islower()):
-            if((ord(letter)+key) > 122):
-                entext += chr((ord(letter)+key)-122 + 96)
-            else:
-                entext += chr(ord(letter)+key)
+        if(letter.islower()):
+            entext += chr((ord(letter)+key-97)%26+97)
         elif(letter.isupper()):
-            if((ord(letter)+key) > 90):
-                entext += chr((ord(letter)+key)-90 + 64)
-            else:
-                entext += chr(ord(letter)+key)
-        elif(letter.isdigit()):
-            entext += letter
-            continue
+            entext += chr((ord(letter)+key-65)%26+65)
         else:
             entext += letter
        
@@ -30,22 +18,10 @@ def decriptions():
     text = input("Text = ")
     detext = ""
     for letter in text:
-        if(letter == " "):
-            detext += letter
-            continue
-        elif(letter.islower()):
-            if((ord(letter)-key) < 97):
-                detext += chr(123 - (97 - (ord(letter)-key)))
-            else:
-                detext += chr(ord(letter)-key)
+        if(letter.islower()):
+            detext += chr((ord(letter)-key-97)%26+97)
         elif(letter.isupper()):
-            if((ord(letter)+key) < 65):
-                detext += chr(91 - (65 - (ord(letter)-key)))
-            else:
-                detext += chr(ord(letter)-key)
-        elif(letter.isdigit()):
-            detext += letter
-            continue
+            detext += chr((ord(letter)-key-65)%26+65)
         else:
             detext += letter
     text = detext
