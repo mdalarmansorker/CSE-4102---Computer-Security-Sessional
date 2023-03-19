c022079b3e38aa91c8416d22662f5291980ece40
Arman <mdalarmansorker@gmail.com>
2023-03-19 14:03:26 +0600

    s box and expension function

--- S_box.py
+++ S_box.py
@@ -1,6 +1,6 @@
-sixth_bit_input = input("6 bit binary input: ")
-sixth_bit_input = list(map(int, sixth_bit_input))
-
+number = int(input("Integer input: "))
+number = format(number, '06b')
+print("Binary:",number)
 s_box = [[2,12,4,1,7,10,11,6,8,5,3,15,13,0,14,9],
         [14,11,2,12,4,7,13,1,5,0,15,10,3,9,8,6],
         [4,2,1,11,10,13,7,8,15,9,12,5,6,3,0,14],
@@ -11,8 +11,8 @@ s_box = [[2,12,4,1,7,10,11,6,8,5,3,15,13,0,14,9],
 #     a = list(map(int, input().split(" ")))
 #     s_box.append(a)
 
-row = 1*sixth_bit_input[5]+2*sixth_bit_input[0]
-column = sixth_bit_input[1]*8+sixth_bit_input[2]*4+sixth_bit_input[3]*2+sixth_bit_input[4]*1;
+row = int(number[5]+number[0], 2)
+column = int(number[4]+number[3]+number[2]+number[1], 2)
 output = '{:04b}'.format(s_box[row][column])
 
-print(output)
+print("Output:",output)
---
+++ expension_function.py
@@ -0,0 +1,17 @@
+# Hexadecimal = hex(input("Hexadecimal input: "))
+# binary = format(Hexadecimal, '032b')
+binary = input("Binary input: ")
+e_table = [32,1,2,3,4,5,
+        4,5,6,7,8,9,
+        8,9,10,11,12,13,
+        12,13,14,15,16,17,
+        16,17,18,19,20,21,
+        20,21,22,23,24,25,
+        24,25,26,27,28,29,
+        28,29,30,31,32,1]
+
+output = ""
+for i in e_table:
+    output += binary[i-1]
+
+print("48 bit conversion: ",output)
